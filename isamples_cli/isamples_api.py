e7f3a6e6151b6a90d4154a128ee0dcdbe8852f0a
datadavev <605409+datadavev@users.noreply.github.com>
2021-12-22 10:59:54 -0500

    Initial implementation

--- isamples_cli/isamples_api.py
+++ isamples_cli/isamples_api.py
@@ -1,10 +1,70 @@
 import logging
 import urllib.parse
 import httpx
+import io
 import asyncio
+import ijson
+
+MAX_STREAMING_ROWS = 500000
+
+
+class StringIteratorIO(io.TextIOBase):
+    # https://gist.github.com/anacrolix/3788413
+
+    def __init__(self, iter):
+        self._iter = iter
+        self._left = ""
+
+    def readable(self):
+        return True
+
+    def _read1(self, n=None):
+        while not self._left:
+            try:
+                self._left = next(self._iter)
+            except StopIteration:
+                break
+        ret = self._left[:n]
+        self._left = self._left[len(ret) :]
+        return ret
+
+    def read(self, n=None):
+        l = []
+        if n is None or n < 0:
+            while True:
+                m = self._read1()
+                if not m:
+                    break
+                l.append(m)
+        else:
+            while n > 0:
+                m = self._read1(n)
+                if not m:
+                    break
+                n -= len(m)
+                l.append(m)
+        return "".join(l)
+
+    def readline(self):
+        l = []
+        while True:
+            i = self._left.find("\n")
+            if i == -1:
+                l.append(self._left)
+                try:
+                    self._left = next(self._iter)
+                except StopIteration:
+                    self._left = ""
+                    break
+            else:
+                l.append(self._left[: i + 1])
+                self._left = self._left[i + 1 :]
+                break
+        return "".join(l)
+
 
 class ISamplesAPI:
-    RECORD_FORMATS = ['core', 'original', 'full', 'solr']
+    RECORD_FORMATS = ["core", "original", "full", "solr"]
 
     def __init__(self, base_url):
         self.base_url = base_url
@@ -17,11 +77,35 @@ class ISamplesAPI:
         params = {"format": fmt}
         return f"{url}?{urllib.parse.urlencode(params)}"
 
-    def thing(self, pid, fmt="core"):
+    async def thing(self, pid, fmt="core"):
         assert fmt in ISamplesAPI.RECORD_FORMATS
         _id = urllib.parse.quote(pid)
         url = f"{self.base_url}/thing/{_id}"
         params = {"format": fmt}
+        async with httpx.AsyncClient() as client:
+            res = await client.get(url, params=params)
+        return (pid, res.json())
+        # res = httpx.get(url, params=params)
+        # return res.json()
+
+    def thing_list_metadata(self):
+        url = self.base_url
+        res = httpx.get(url)
+        return res.json()
+
+    def thing_types(self):
+        url = f"{self.base_url}/"
+        res = httpx.get(url)
+        return res.json()
+
+    def thing_list(self, offset=0, limit=1000, status=200, authority=None):
+        url = f"{self.base_url}/"
+        params = {
+            "offset": offset,
+            "limit": limit,
+            "status": status,
+            "authority": authority,
+        }
         res = httpx.get(url, params=params)
         return res.json()
 
@@ -33,19 +117,85 @@ class ISamplesAPI:
         res = httpx.get(url, params=params)
         return res.json()
 
+    def thing_select_info(self):
+        url = f"{self.base_url}/thing/select/info"
+        res = httpx.get(url)
+        return res.json()
+
+    def thing_stream(
+        self,
+        q: str = "*:*",
+        rows: int = MAX_STREAMING_ROWS,
+        offset: int = 0,
+        xy_count: bool = False,
+        fq: list = None,
+        fl: list = None,
+        random_sel: bool = False,
+        sort: dict = None,
+    ):
+
+        url = f"{self.base_url}/thing/stream"
+        params = {
+            "q": q,
+            "rows": rows,
+            "offset": offset,
+            "xycount": xy_count,
+        }
+        if fq is not None:
+            params["fq"] = fq
+        if fl is not None:
+            params["fl"] = ",".join(fl)
+        if sort is not None:
+            params["sort"] = sort
+        if random_sel:
+            params["select"] = "random"
+        with httpx.stream("GET", url, params=params) as response:
+            for record in ijson.items(
+                StringIteratorIO(response.iter_text()), "result-set.docs.item", use_float=True
+            ):
+                yield record
+
+    def things_geojson_heatmap(
+        self,
+        query="*:*",
+        fq=None,
+        min_lat=-90.0,
+        max_lat=90.0,
+        min_lon=-180.0,
+        max_lon=180.0,
+    ):
+        raise NotImplementedError()
+
+    def things_leaflet_heatmap(
+        self,
+        query="*:*",
+        fq=None,
+        min_lat=-90.0,
+        max_lat=90.0,
+        min_lon=-180.0,
+        max_lon=180.0,
+    ):
+        raise NotImplementedError()
+
     # API convenience methods
     def getIDs(self, q="*:*", fq=None, rows=10, offset=0):
         res = self.thing_select(q=q, fq=fq, rows=rows, offset=offset, fields="id")
         for doc in res.get("response", {}).get("docs", []):
             yield doc.get("id", None)
 
-    async def _getUrl(self, url):
-        async with httpx.AsyncClient() as client:
-            res = await client.get(url)
-            return res.json()
+    async def getRecords(self, pids, format="core"):
+        """
+        Retrieve the records in the specified format.
 
-    async def getRecords(self, pids, format='core'):
-        urls = []
-        for pid in pids:
-            urls.append(self.thing_url(pid, format))
-        return await asyncio.gather(*map(self._getUrl, urls))
+        Args:
+            pids: List of Identifiers for the records
+            format: Name of a supported record format
+
+        Returns:
+            iterator of promises that resolve to (pid, record) tuples
+        """
+
+        def _rmap(pid):
+            return self.thing(pid, format)
+
+        return await asyncio.gather(*map(_rmap, pids))
--- isamples_cli/main.py
+++ isamples_cli/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import click
 import logging
@@ -6,8 +7,50 @@ import urllib.parse
 import json
 import asyncio
 
-SERVICES = [{"name": ["mars", "dev"], "url": "https://mars.cyverse.org"}]
-RECORD_FORMATS = ['core', 'original', 'full', 'solr']
+from isamples_cli import isamples_api
+
+# List of services. This should be dynamic, probably from github.
+# URLs should not include a trailing slash
+SERVICES = [
+    {"name": ["mars", "dev"], "url": "https://mars.cyverse.org"},
+    {"name": ["opencontext", "oc"], "url": "https://henry.cyverse.org/opencontext"},
+    {"name": ["smithsonian", "si"], "url": "https://henry.cyverse.org/smithsonian"},
+    {
+        "name": [
+            "geome",
+        ],
+        "url": "https://henry.cyverse.org/geome",
+    },
+    {
+        "name": [
+            "sesar",
+        ],
+        "url": "https://henry.cyverse.org/sesar",
+    },
+    {"name": ["central", "isc"], "url": "https://hyde.cyverse.org"},
+    {
+        "name": [
+            "local",
+        ],
+        "url": "http://localhost:8000",
+    },
+]
+RECORD_FORMATS = ["core", "original", "full", "solr"]
+
+FNAME_CHAR_REPLACEMENT = [(":", "_"), ("/", "~"), ("\\", "~")]
+
+# enable -h for help
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+# Names for log levels
+LOGLEVELS = {
+    "critical": logging.CRITICAL,
+    "error": logging.ERROR,
+    "warn": logging.WARNING,
+    "warning": logging.WARNING,
+    "info": logging.INFO,
+    "debug": logging.DEBUG,
+}
 
 L = logging.getLogger("iSample")
 
@@ -19,14 +62,24 @@ def getServiceByName(name):
     return None
 
 
+def identifierToFileName(pid, path="."):
+    fname = pid
+    for a, b in FNAME_CHAR_REPLACEMENT:
+        fname = fname.replace(a, b)
+    final_name = fname
+    cntr = 1
+    while os.path.exists(os.path.join(path, final_name)):
+        final_name = f"{fname}-{cntr}"
+    return final_name
 
 
 @click.group()
 @click.pass_context
-def main(ctx):
+@click.option("-L", "--LOGLEVEL", help="Logging level", default="info")
+def main(ctx, loglevel):
     lformat = "%(levelname)s:%(name)s %(message)s"
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=LOGLEVELS.get(loglevel, "info"),
         format=lformat,
         datefmt="%Y-%m-%d %H:%M:%S",
     )
@@ -34,7 +87,52 @@ def main(ctx):
     return 0
 
 
-@main.command()
+@main.command(name="services", context_settings=CONTEXT_SETTINGS)
+@click.pass_context
+def listServices(ctx):
+    """
+    List the available services
+    """
+    for svc in SERVICES:
+        print(", ".join(svc.get("name", [])))
+        print(f"  {svc.get('url', '')}")
+
+
+@main.command(name="fields", context_settings=CONTEXT_SETTINGS)
+@click.pass_context
+@click.argument("service")
+def listFields(ctx, service):
+    svc = getServiceByName(service)
+    if svc is None:
+        L.error("Unknown service: '%s'", service)
+        return 1
+    api = isamples_api.ISamplesAPI(svc.get("url"))
+    res = api.thing_select_info()
+    fields = res["schema"]["fields"]
+    for k, v in fields.items():
+        print(f"{k} ({v['type']}) {v['flags']}")
+    # print(json.dumps(api.thing_select_info(), indent=2))
+
+
+@main.command(name="pids", context_settings=CONTEXT_SETTINGS)
+@click.pass_context
+@click.argument("service")
+@click.option("-q", "--query", help="Solr query to filter records", default="*:*")
+@click.option(
+    "-n", "--numrecs", help="Number of records to retrieve", default=10, type=int
+)
+def getIdentifiers(ctx, service, query, numrecs):
+    """ """
+    svc = getServiceByName(service)
+    if svc is None:
+        L.error("Unknown service: '%s'", service)
+        return 1
+    api = isamples_api.ISamplesAPI(svc.get("url"))
+    for pid in api.getIDs(q=query, rows=numrecs):
+        print(pid)
+
+
+@main.command(name="records", context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument("service")
 @click.option("-q", "--query", help="Solr query to filter records", default="*:*")
@@ -42,23 +140,72 @@ def main(ctx):
     "-n", "--numrecs", help="Number of records to retrieve", default=10, type=int
 )
 @click.option(
-    "-f",
-    "--format",
-    help="Record format",
+    "-m",
+    "--model",
+    help="Record model",
     type=click.Choice(RECORD_FORMATS),
-    default="core"
+    default="core",
 )
-def getRecords(ctx, service, query, numrecs, format):
+@click.option("-d", "--destfolder", help="Folder for saving records", default=None)
+def getRecords(ctx, service, query, numrecs, model, destfolder):
+    """
+    Retrieve records from SERVICE
+
+    SERVICE is the name of an iSamples endpoint
+
+    Output is to stdout unless DESTFOLDER is specified, in which case
+    individual JSON records are written to the folder with an additional
+    index.json file that contains the mapping between filename and PID.
+    """
     svc = getServiceByName(service)
     if svc is None:
         L.error("Unknown service: '%s'", service)
         return 1
-    api = ISamplesAPI(svc.get("url"))
+    if destfolder is not None:
+        os.makedirs(destfolder, exist_ok=True)
+    api = isamples_api.ISamplesAPI(svc.get("url"))
     _ids = list(api.getIDs(q=query, rows=numrecs))
-    print(json.dumps(asyncio.run( api.getRecords(_ids, format=format)), indent=2))
-    #for _id in api.getIDs(q=query, rows=numrecs):
-    #    L.info(_id)
-    #    print(json.dumps(api.thing(_id, fmt=format), indent=2))
+    _index = {}
+    counter = 0
+    for (pid, rec) in asyncio.run(api.getRecords(_ids, format=model)):
+        counter += 1
+        if destfolder is None:
+            print(json.dumps(rec, indent=2))
+        else:
+            _index[pid] = identifierToFileName(pid, path=destfolder)
+            with open(os.path.join(destfolder, _index[pid]), "w") as destf:
+                json.dump(rec, destf, indent=2)
+    if destfolder is not None:
+        with open(os.path.join(destfolder, "index.json"), "w") as destf:
+            json.dump(_index, destf, indent=2)
+    L.info("Retrieved %s records", counter)
+
+
+@main.command(name="stream", context_settings=CONTEXT_SETTINGS)
+@click.pass_context
+@click.argument("service")
+@click.option("-q", "--query", help="Solr query to filter records", default="*:*")
+@click.option(
+    "-n", "--numrecs", help="Number of records to retrieve", default=10, type=int
+)
+@click.option("-f", "--fields", help="Fields to return", default=None)
+@click.option("-r", "--random_sel", help="Random selection of records", is_flag=True)
+@click.option(
+    "--xycount", help="Aggregate by longitude, latitude and return count", is_flag=True
+)
+def getStream(ctx, service, query, numrecs, fields, random_sel, xycount):
+    svc = getServiceByName(service)
+    if svc is None:
+        L.error("Unknown service: '%s'", service)
+        return 1
+    fl = None
+    if fields is not None:
+        fl = fields.split(",")
+    api = isamples_api.ISamplesAPI(svc.get("url"))
+    for record in api.thing_stream(
+        q=query, rows=numrecs, fl=fl, random_sel=random_sel, xy_count=xycount
+    ):
+        print(json.dumps(record))
 
 
 if __name__ == "__main__":
